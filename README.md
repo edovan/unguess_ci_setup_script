3e047d03975f3d6716ccab25a02ff9dd0f6a6834
Edoardo Vannutelli <edoardo.vannutelli@gmail.com>
2022-04-21 17:01:05 +0200

    Update script
    switced form env variables to commad line parameter

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # UNGUESS Ci Setup
 
 this repo contasins the scipts to setup a clowd execution of tests
+
+## Requirments 
+In oder to run this script you must have 
+
+1. AWS cli installed 
+1. An aws account configured on your machine 
+1. Your account should have access to manange 
+    1. EC2 
+    1. ECR 
+    1. S3
+
+## Setup 
+
+
+
+to run the script you shoud 
+
+``` bash 
+> setup_aws.sh   -c Cliente -p Prodotto -r Regione
+``` 
--- script/setup_aws.sh
+++ script/setup_aws.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
+set -x
+while getopts c:p:r: flag
+do
+    case "${flag}" in
+        c) customerName=${OPTARG};;
+        p) productName=${OPTARG};;
+        r) regionName=${OPTARG};;
+    esac
+done
+echo "Customer Name: $customerName";
+echo "Product Name: $productName";
+echo "Regione Name: $regionName";
+ 
 # <========== GENERAL SETUP  ==========> #
-_CUSTOMER_NAME=$(tr '[:upper:]' '[:lower:]'  <<<  $CUSTOMER_NAME)
-_PRODUCT_NAME=$(tr '[:upper:]' '[:lower:]'  <<<  $PRODUCT_NAME)
-_AWS_REGION=$(tr '[:upper:]' '[:lower:]'  <<<  $AWS_REGION)
+_CUSTOMER_NAME=$(tr '[:upper:]' '[:lower:]'  <<<  $customerName)
+_PRODUCT_NAME=$(tr '[:upper:]' '[:lower:]'  <<<  $productName)
+_AWS_REGION=$(tr '[:upper:]' '[:lower:]'  <<<  $regionName)
 # <====================================> #
 
+
+echo "Customer Name: $_CUSTOMER_NAME";
+echo "Product Name: $_PRODUCT_NAME";
+echo "Regione Name: $_AWS_REGION";
 ### TODO:
 # 1. Create ECR repository 
 # 2. Create Results Bucket
@@ -17,10 +34,10 @@ aws ecr get-login-password --region ${_AWS_REGION} | docker login --username AWS
 aws ecr create-repository \
     --repository-name e2e_${_CUSTOMER_NAME}_${_PRODUCT_NAME} \
     --image-scanning-configuration scanOnPush=true \
-    --region ${_AWS_REGION}
+    --region $_AWS_REGION
 
 ## 2. Create S3 Bucket 
-aws s3 mb s3://e2e-${_CUSTOMER_NAME}-${_PRODUCT_NAME}_repo
+aws s3 mb s3://e2e-${_CUSTOMER_NAME}-${_PRODUCT_NAME}-repo  --region $_AWS_REGION
 
 ## 3. Create Execution Cluster 
-aws ecs create-cluster --cluster-name e2e-${_CUSTOMER_NAME}-${_PRODUCT_NAME}
+aws ecs create-cluster --cluster-name e2e-$_CUSTOMER_NAME-$_PRODUCT_NAME  --region $_AWS_REGION
